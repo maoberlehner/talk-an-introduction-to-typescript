d8df1dc3bdd28e0dab1d68a3f1746a0ed0cff1b5
Markus Oberlehner <markus.oberlehner@karriere.at>
2017-05-30 08:23:15 +0200

    Add tslint disable comments for not allowed console.log

--- src/classes/ElectroEngine.ts
+++ src/classes/ElectroEngine.ts
@@ -4,6 +4,7 @@ export class ElectroEngine implements IEngine {
   constructor(public kw: number) { }
 
   public start() {
+    // tslint:disable-next-line
     console.log(`A ${this.kw}kw electro engine makes: Fip Fip`);
   }
 }
--- src/classes/GasolineEngine.ts
+++ src/classes/GasolineEngine.ts
@@ -4,6 +4,7 @@ export class GasolineEngine implements IEngine {
   constructor(public kw: number) { }
 
   public start() {
+    // tslint:disable-next-line
     console.log(`A ${this.kw}kw gasoline engine makes: Brum Brum`);
   }
 }
